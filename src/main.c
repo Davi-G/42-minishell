bdba2a5621fdee5a4bacf20fd4d03cc7db517bd6
Davi-G <davidgomezvillanueva@gmail.com>
2024-04-25 17:58:31 +0200

    added home

--- src/main.c
+++ src/main.c
@@ -6,38 +6,45 @@
 /*   By: dagomez <dagomez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 20:00:36 by davi-g            #+#    #+#             */
-/*   Updated: 2024/04/25 17:20:45 by dagomez          ###   ########.fr       */
+/*   Updated: 2024/04/25 17:55:14 by dagomez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/* 
+
 static char	*home_finder(t_master *control)
 {
 	int		i;
+	int		j;
 	char	*home;
 
 	i = 0;
 	home = NULL;
+	control->old_pwd = getcwd(0, 0);
 	while (control->old_pwd[i])
 		i++;
-	int j = i;
+	j = i;
 	while (control->old_pwd[i] != '/')
 		i--;
-	home = malloc(sizeof(char) * (j - i + 1));
+	i++;
+	home = malloc(sizeof(char) * (j - i));
+	j = 0;
 	while (control->old_pwd[i])
 	{
-		home[i] = control->old_pwd[i];
+		home[j] = control->old_pwd[i];
 		i++;
+		j++;
 	}
-	printf("home: %s\n", home);
+	home[j] = '-';
+	home[j + 1] = '>';
+	home[j + 2] = '$';
 	return (home);
-} */
+}
 
 int	main(int ac, char **av, char **env)
 {
 	char		*out;
-	//char		*home;
+	char		*home;
 	t_master	control;
 	t_data		info;
 
@@ -46,11 +53,14 @@ int	main(int ac, char **av, char **env)
 	using_history();
 	control.exit_status = 0;
 	out = 0;
-	//home = home_finder(&control);
 	while (control.exit_status != 1)
 	{
+		home = home_finder(&control);
+		home = ft_strjoin(MAGENTA, home);
+		home = ft_strjoin(home, RESET);
 		control.old_pwd = getcwd(0, 0);
-		out = readline(MAGENTA"minishell$ "RESET);	
+		out = readline(home);
+		//out = readline(MAGENTA"ms$ "RESET);	
 		if (ft_strlen(out) > 0)
 			add_history(out);
 		info = parser(out);
